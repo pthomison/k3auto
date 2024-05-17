ee983361e2f8d0d7f34d44a9cf3184b4ade1d1d6
pthomison <p.thomison@gmail.com>
2024-05-16 21:50:59 -0700

    dynamic image

--- pkg/k3auto/config.go
+++ pkg/k3auto/config.go
@@ -1,7 +1,11 @@
 package k3auto
 
 import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
 	k3dv1alpha5 "github.com/k3d-io/k3d/v5/pkg/config/v1alpha5"
+	k3dversion "github.com/k3d-io/k3d/v5/version"
 	defaults "github.com/pthomison/k3auto/default"
 	"github.com/pthomison/k3auto/internal/k3d"
 	"github.com/spf13/afero"
@@ -28,11 +32,14 @@ func ParseK3dConfigFile(configPath string) (*k3dv1alpha5.SimpleConfig, error) {
 		if err != nil {
 			return nil, err
 		}
+
 	} else {
 		clusterConfig, err = k3d.ParseConfigFile(defaults.K3dConfigLocation, &defaults.K3dConfig)
 		if err != nil {
 			return nil, err
 		}
+		clusterConfig.Image = fmt.Sprintf("docker.io/rancher/k3s:%v", k3dversion.K3sVersion)
+		spew.Dump(clusterConfig.Image)
 	}
 
 	return clusterConfig, nil
