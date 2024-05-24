4c3490fdae5ec7133162e06329466da2d377072f
pthomison <p.thomison@gmail.com>
2024-05-24 09:21:12 -0700

    export improvements

--- cmd/export.go
+++ cmd/export.go
@@ -5,9 +5,10 @@ import (
 	"errors"
 	"io/fs"
 	"path/filepath"
+	"strings"
 
-	"github.com/davecgh/go-spew/spew"
 	defaults "github.com/pthomison/k3auto/default"
+	"github.com/pthomison/k3auto/internal/flux"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 )
@@ -40,21 +41,36 @@ func k3AutoExport(cmd *cobra.Command, args []string) {
 		checkError(err)
 
 		p := filepath.Join(exportDirectory, path)
-		spew.Dump(p)
+		cp := filepath.Clean(strings.Replace(p, defaults.DefaultDeploymentsFolder, "", -1))
 
 		if !info.IsDir() {
-			b, err2 := afero.ReadFile(embedfs, path)
-			checkError(err2)
+			b, err := afero.ReadFile(embedfs, path)
+			checkError(err)
 
-			err2 = afero.WriteFile(osfs, p, b, info.Mode())
-			checkError(err2)
+			if exists, _ := afero.Exists(osfs, cp); exists {
+				err = osfs.Remove(cp)
+				checkError(err)
+			}
+
+			err = afero.WriteFile(osfs, cp, b, info.Mode())
+			checkError(err)
 		} else {
-			err2 := osfs.Mkdir(p, 0755)
-			if !errors.Is(err2, afero.ErrFileExists) {
-				checkError(err2)
+			err := osfs.Mkdir(cp, 0755)
+			if !errors.Is(err, afero.ErrFileExists) {
+				checkError(err)
 			}
 		}
 
 		return nil
 	})
+
+	manifests, err := flux.GenerateManifests("v2.3.0")
+	checkError(err)
+
+	p := filepath.Join(exportDirectory, manifests.Path)
+
+	osfs.MkdirAll(filepath.Dir(p), 0755)
+
+	err = afero.WriteFile(osfs, p, []byte(manifests.Content), 0644)
+	checkError(err)
 }
